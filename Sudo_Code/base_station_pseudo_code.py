514bc15b0d1db0399de350bcb26afcf3d59277ff
Evan Bartel <47606639+EvanBartel@users.noreply.github.com>
2024-10-09 15:51:08 -0400

    initial psuedo code for user terminal and upfate base_station

--- Sudo_Code/base_station_pseudo_code.py
+++ Sudo_Code/base_station_pseudo_code.py
@@ -1,46 +1,40 @@
+# Base Station Pseudocode – Analogous to State Diagram
 if __name__ == "__main__":
     # Base Station's been turned on
     router = startRoutingService(); # Routing Service handles the Network Traffic
-    server = startServer();         # Server handles message passing from User to Base
+    server = startServer();         # Server handles User/Base message passing
     vectorSystem = initVector();    # Initialize Tracking System
+    userQueue = queue();            # Queue to keep track of users
 
-    # Keep track of users
-    userQueue = queue();                # Empty Queue
-
-    ## Server Functions ##
+    ## Server Callback Functions ##
     server.onUserConnect = lambda (newUser):
         # When user connects...
         userQueue.add(newUser);         # Add user to client queue
-        vectorSystem.process(newUser);  # Process Calculations for 
-                                        # new user
+        vectorSystem.process(newUser);  # Process calculations for new user
 
     server.onUserMessage = lambda (msg, user):
-        # user sends a msg...
+        # user sends a msg/query...
         if (msg == "NETHEALTH?"):
             networkHealth = router.getNetworkHealth();
             server.send(user, location);
-
         if (msg == "LOCATION?"):
-            location = vectorSystem.track(user, userQueue);    
+            location = vectorSystem.track(user, userQueue);
             server.send(user, location);
-    
         if (msg == "WEBSITE?"):
-            server.serveWebsite(user); # HTTP request. Handled by a separate process,
-                            # It's placed here for the sake of simplicity
-
-    ## Repeated, timed actions ##
+            server.serveWebsite(user);	# “HTTP Request”
+                            		 	# Placed here for simplicity
+    ## Repeated, Timed Callback Functions ##
     timer.trackingTimer = lambda():
         # Every X sec...
-        currLocations = vectorSystem.track(userQueue);      # First estimate of position via 
-                            # Channel State Information. DoA & Time of Flight provides Angle & 
-                            # Radial Distance. Data is not filtered.
-        filtLocations = vectorSystem.filter(currLocations); # Filter data with past location
-                            # Information.
-        vectorSystem.updateHistory(filtLocations);          # Store filtered data.
-        server.broadcast(userQueue, filtLocations);             # Broadcast Tracking Data to users.
+        currLocations = vectorSystem.track(userQueue);
+                            # Use Channel State Information for Position
+                            # Determination. Data is not filtered.
+        filtLocations = vectorSystem.filter(currLocations);
+ 			   # Filter data with past location information
+        vectorSystem.updateHistory(filtLocations);   # Store location info
+        server.broadcast(userQueue, filtLocations);  # Broadcast Location
 
     timer.broadcastTimer = lambda ():
-        # Every X amount of time...
+        # Every X minutes...
         networkHealth = router.getNetworkHealth();              # Assess.
-        server.broadcast(userQueue, networkHealth);                 # Serve.
-        
+        server.broadcast(userQueue, networkHealth);             # Serve.
--- Sudo_Code/user_terminal_pseudo_code.py
+++ Sudo_Code/user_terminal_pseudo_code.py
@@ -1,46 +1,69 @@
-def turn_on():
-    # Turn on the user terminal
-    user_terminal.turn_on()
-
-def connect_to_base_station():
-    # Connect to the base station
-    user_terminal.connect()
-
-def display_new_nav_information():
-    # Display the new navigation information
-    user_terminal.display()
-
-def respond_to_base_station():
-    # Respond to the base station
-    user_terminal.respond()
-
-def make_old_nav_information_stale():
-    # Make old navigation information
-    user_terminal.make_stale()
-
-def receive_data_from_base_station():
-    # Receive data from the base station
-    return user_terminal.receive_data()
-
-def disconnect_from_base_station():
-    # Disconnect from the base station
-    user_terminal.disconnect()
-
-
+# User Terminal Pseudocode - Analogous to State Diagram
 if __name__ == "__main__":
     # User Terminal's been turned on
-    
-    # Connect to the base station
-    connect_to_base_station()
+    userTerminal = startUserTerminal(); # Handler for the User's Terminal
 
-    # Display the new navigation information
-    display_new_nav_information()
+    # Keep trying to connect to the Router until it's connected
+    router = connectToRouter();
+    while not router.isConnected():
+        router = connectToRouter(); # Connect to the Router
+        # return router/wifi handle (with connection status)
 
-    if receive_data_from_base_station():
-        # Make old navigation information\
+    # Attempt connection to Server
+    server = connectToServer(SERVERIP);
+    while not server.isConnected():
+        server, connected = connectToServer(SERVERIP);
+        # return server handle (with connection status)
 
-        make_old_nav_information_stale()
-    # Respond to the base station
-    respond_to_base_station()
+    # Keep track of system data points (Nethealth, Location, etc.)
+    systemData = queue();
+
+    ## UI Input Functions ##
+    userTerminal.onButtonPress = lambda (btn):
+        # User Terminal Button Pressed
+        # Send a msg to server to request:
+        if (btn == "A"):
+            server.send("NETHEALTH?"); # Request Network Health
+
+        if (btn == "B"):
+            server.send("LOCATION?");  # Request Location
+
+    ## Client/ServerFunctions ##
+    server.onSendError = lambda (error, msg):
+        # Error in sending message (due to channel, etc.)
+        userTerminal.display(error, msg); # Show error occurred
+        server.send(msg);                 # Resend message.
+
+    server.onServerMessage = lambda (msg):
+        # Message received from server
+        if (msg.type == "NETHEALTH!"):
+            dataQueue.push(msg.data);       # Process.
+            userTerminal.display(msg.data); # Display.
+
+        if (msg.type == "LOCATION!"):
+            dataQueue.push(data.data);      # Process.
+            userTerminal.display(msg.data); # Display.
+            reset(timer.staleTimer);        # Reset 'Stale' Timer
+
+    ## Repeated, timed actions ##
+    timer.staleTimer = lambda():
+        # Every X sec...
+        # When location data becomes stale, request fresh data
+        server.send("LOCATION?");
+        reset(timer.staleTimer);
 
 
+"""
+The below code describes the behavior of a website connected to the Base Station,
+  but operating on a phone, laptop, etc. instead of a User Terminal
+"""
+# Client connects via web browser (connect to SERVERIP on HTTP port)
+# (represented in Base Station pseudocode by:
+# if (msg == "WEBSITE?"): server.serveWebsite(user)
+while connected:
+    # After receiving the website, connect to server
+    server = connectToServer(SERVERIP);
+    # Wait for broadcasts from server (with network health, locations, etc.)
+    server.onServerMessage = lambda (msg):
+        # Message received from server
+        website.display(msg);   # Dislay found data on website
