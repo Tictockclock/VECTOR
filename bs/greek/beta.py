4398f95c1beeebac93ba04f22d8f221063899dfe
TicTockclock <evanbartel6@gmail.com>
2025-04-08 20:48:18 -0400

    update picoscenes command

--- bs/greek/beta.py
+++ bs/greek/beta.py
@@ -121,7 +121,7 @@ def hack_rf_pinging2():
     in a loop until the `shutdown_event` is set, allowing for graceful termination.
     """
 
-    cmd = f"""PicoScenes \"-d debug -i hackrf0 --freq {config["hack_rf"]["freq"]} --rate {config["hack_rf"]["rate"]} --mode {config["hack_rf"]["mode"]} --repeat {config["hack_rf"]["repeat"]} --delay {config["hack_rf"]["delay"]} --preset {config["hack_rf"]["preset"]}\""""
+    cmd = f"""PicoScenes \"-d debug -i hackrf0 --freq {config["hack_rf"]["freq"]} --rate {config["hack_rf"]["rate"]} --mode {config["hack_rf"]["mode"]} --repeat {config["hack_rf"]["repeat"]} --delay {config["hack_rf"]["delay"]} --preset {config["hack_rf"]["preset"]}; -i 2 --mode logger  --output BETA_HOTSPOT_CSI \""""
 
     print("Running setup command:")
     print(cmd)
@@ -319,18 +319,18 @@ def master_handler():
     # Start the PicoScenes process
     time.sleep(5)
 
-    hack_rf_pinging_thread.start()
-    hack_rf_pinging_thread.join()
+    # hack_rf_pinging_thread.start()
+    # hack_rf_pinging_thread.join()
 
     hack_rf_pinging2_thread.start()
     hack_rf_pinging2_thread.join()
 
     # Start the ZeroMQ file server
-    zmq_thread = threading.Thread(target=start_zmq_file_server)
-    zmq_thread.start()
-    #send message to laptop and alpha
-    send_message_laptop("Stop")
-    send_message_alpha("Stop")
+    # zmq_thread = threading.Thread(target=start_zmq_file_server)
+    # zmq_thread.start()
+    # #send message to laptop and alpha
+    # send_message_laptop("Stop")
+    # send_message_alpha("Stop")
 
     #parse the data
 
